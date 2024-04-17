e44a12c05453b5315855160a8a789f6c81b57576
chill <carbonfiberdev@gmail.com>
2024-04-17 21:03:47 +0300

    refactor
    some refactor

--- src/main/java/com/example/todotest/controller/TodoController.java
+++ src/main/java/com/example/todotest/controller/TodoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import com.example.todotest.repository.TodoEntityRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoController {
@@ -17,17 +18,29 @@ public class TodoController {
 
     @PostMapping("/api/create_todo")
     private ResponseEntity<String> createTodo(@RequestBody TodoDto todoDto){
-        TodoEntity todoEntity = new TodoEntity();
-        todoEntity.setTodoName(todoDto.getTodoName());
-        todoEntity.setTodoStrings(todoDto.getItems());
+        try{
+            Optional<TodoEntity> todoEntityOptional = Optional.ofNullable(todoEntityRepository.findByTodoName(todoDto.getTodoName()));
+            if(todoEntityOptional.isPresent())
+                return new ResponseEntity<>("Todo already exists", HttpStatus.NOT_ACCEPTABLE);
 
-        todoEntityRepository.save(todoEntity);
-        return new ResponseEntity<>("Todo has been created", HttpStatus.CREATED);
+            TodoEntity todoEntity = new TodoEntity();
+            todoEntity.setTodoName(todoDto.getTodoName());
+            todoEntity.setTodoStrings(todoDto.getItems());
+
+            todoEntityRepository.save(todoEntity);
+
+            return new ResponseEntity<>("Todo has been created", HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
+        }
     }
     @GetMapping("/api/get_todo")
-    private ResponseEntity<List<String>> getTodo(@RequestParam String todoName){
-        TodoEntity todoEntity = todoEntityRepository.findByTodoName(todoName);
-
-        return new ResponseEntity<>(todoEntity.getTodoStrings(), HttpStatus.CREATED);
+    private ResponseEntity<?> getTodo(@RequestParam String todoName){
+        try{
+            TodoEntity todoEntity = todoEntityRepository.findByTodoName(todoName);
+            return new ResponseEntity<>(todoEntity, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(todoName+ " Not found", HttpStatus.NOT_FOUND);
+        }
     }
 }
