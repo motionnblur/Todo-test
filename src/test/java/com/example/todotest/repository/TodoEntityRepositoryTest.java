3efcee77c1557305d5072e4a6104a3e8cd3049d9
chill <chill@mail.com>
2024-04-19 22:01:09 +0300

    add
    added real postgresql testing

--- src/test/java/com/example/todotest/repository/TodoEntityRepositoryTest.java
+++ src/test/java/com/example/todotest/repository/TodoEntityRepositoryTest.java
@@ -1,24 +1,52 @@
 package com.example.todotest.repository;
 
 import com.example.todotest.entity.TodoEntity;
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.MountableFile;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 class TodoEntityRepositoryTest {
+    static PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer("postgres:15")
+            .withDatabaseName("test")
+            .withUsername("root")
+            .withPassword("root");
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresqlContainer::getUsername);
+        registry.add("spring.datasource.password", postgresqlContainer::getPassword);
+    }
+    @BeforeAll
+    static void beforeAll() {
+        postgresqlContainer.start();
+    }
+    @AfterAll
+    static void afterAll() {
+        postgresqlContainer.stop();
+    }
+
     @Autowired
     TodoEntityRepository todoEntityRepository;
-
     @Test
     void shouldSaveTodoToDatabase() {
         List<String> todoItems = new ArrayList<>();
